47606632a133d4429a00e4327c5c030b9357c86c
Fylls <sergentifilippo@gmail.com>
2020-06-01 17:49:05 +0200

    236
    Signing Users Up

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,6 +1,13 @@
+// token based authentication approach
+// creating a token which I return to my client side app, to the react app which then could
+// theoretically handle that token to authenticate itself to my backend for future requests
+
 const Router = require("express").Router;
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
+
+const mongodb = require("mongodb");
+const db = require("../db");
 
 const router = Router();
 
@@ -28,17 +35,28 @@ router.post("/signup", (req, res, next) => {
     .hash(pw, 12)
     .then((hashedPW) => {
       // Store hashedPW in database
-      console.log(hashedPW);
-      const token = createToken();
-      res
-        .status(201)
-        .json({ token: token, user: { email: "dummy@dummy.com" } });
+
+      db.getDB()
+        .db()
+        .collection("users")
+        .insertOne({
+          email: email,
+          password: hashedPW,
+        })
+        .then((result) => {
+          console.log(result);
+          const token = createToken();
+          res.status(201).json({ token: token, user: { email: email } });
+        })
+        .catch((err) => {
+          console.log(err);
+          res.status(500).json({ message: "Creating the user failed." });
+        });
     })
     .catch((err) => {
       console.log(err);
       res.status(500).json({ message: "Creating the user failed." });
     });
-  // Add user to database
 });
 
 module.exports = router;
